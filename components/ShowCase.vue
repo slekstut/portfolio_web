24480228945023cf3508305634d6ade333074dfb
Šarūnas <52723739+slekstut@users.noreply.github.com>
2023-03-08 21:27:39 +0200

    add images and start showcases

--- components/ShowCase.vue
+++ components/ShowCase.vue
@@ -4,7 +4,11 @@
         <div class="container" ref="container">
             <h5 class="section__title" ref="sectionTitle">showcase</h5>
             <div class="section__body">
-           
+                <div v-for="(showCase, index) in showCases" :key="index">
+                    <div>
+                        <h3>{{showCase.title}}</h3>
+                    </div>
+                </div>
             </div>
         </div>
         <div class="side__img">
@@ -23,20 +27,30 @@ export default {
                 title: 'Personal Portfolio',
                 description: 'Design and Development',
                 tech: 'vue js, nuxt, scss, typescript',
-                image: 'https://example.com/image1.jpg',
+                image: '~assets/img/project1.png',
                 url: 'https://example.com/project1'
             },
             {
                 id: 2,
-                title: 'Project 2',
-                description: 'This is the description of project 2',
+                title: 'url shortening api',
+                description: 'design and development',
+                tech: 'vue js, nuxt, scss',
                 image: 'https://example.com/image2.jpg',
                 url: 'https://example.com/project2'
             },
             {
                 id: 3,
-                title: 'Project 3',
-                description: 'This is the description of project 3',
+                title: 'IP address tracker',
+                description: 'design and development',
+                tech: 'vue js, nuxt, scss ',
+                image: 'https://example.com/image3.jpg',
+                url: 'https://example.com/project3'
+            },
+            {
+                id: 4,
+                title: 'crud app',
+                description: 'design and development',
+                tech: 'vue js, nuxt, scss ',
                 image: 'https://example.com/image3.jpg',
                 url: 'https://example.com/project3'
             }
