5286e359cdb10c7983b519b845fa37ec92809b8b
Šarūnas <52723739+slekstut@users.noreply.github.com>
2023-03-12 21:52:40 +0200

    add index page

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <App />
+    </div>
+</template>
+
+<script lang="ts">
+import App from '../app.vue'
+export default {
+    components: {
+        App
+    }
+}
+</script>
